2bd6d9432dfaf7b8fe8bbc2d0ad0f6397fe5133a
1355165054@qq.com <1355165054@gmail.com>
2024-12-16 13:31:26 +0800

    修改lwidget部分内容

--- src/core/luicommon/lwidget.cpp
+++ src/core/luicommon/lwidget.cpp
@@ -1,10 +1,76 @@
 #include "lwidget.h"
-
+#include "ltitlebar.h"
 LWidget::LWidget(QWidget * parent /*= nullptr*/)
     : QWidget(parent)
+    , m_pTitleBar(new LTitleBar)
 {
+    // Qt::FramelessWindowHint 无边框
+    // Qt::WindowStaysOnTopHint 窗口在最顶端，不会拖到任务栏下面
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowMinimizeButtonHint);
+    initWidget();
+
+    connectInit();
 }
 
 LWidget::~LWidget()
 {
+    // 清理资源
+    if (m_pTitleBar)
+    {
+        delete m_pTitleBar;
+        m_pTitleBar = nullptr;
+    }
+    if (m_pPalletWidget)
+    {
+        delete m_pPalletWidget;
+        m_pPalletWidget = nullptr;
+    }
+}
+
+void LWidget::initWidget()
+{
+}
+
+void LWidget::setWidgetState(WidgetState widgetState)
+{
+    m_currentWidgetState = widgetState;
+}
+
+void LWidget::connectInit()
+{
+    connect(m_pTitleBar, &LTitleBar::btnCloseClicked, this, &LWidget::onTitleBarBtnCloseClicked);
+    connect(m_pTitleBar, &LTitleBar::btnMinClicked, this, &LWidget::onTitleBarBtnMinClicked);
+    connect(m_pTitleBar, &LTitleBar::btnMaxOrNomalClicked, this, &LWidget::onTitleBarBtnNomalOrMaxClicked);
+    connect(m_pTitleBar, &LTitleBar::titleBarDoubleClicked, this, &LWidget::onTitleBarDoubleClicked);
+}
+
+void LWidget::onTitleBarBtnCloseClicked()
+{
+    close();
+}
+
+void LWidget::onTitleBarBtnMinClicked()
+{
+    showMinimized();
+}
+
+void LWidget::onTitleBarBtnNomalOrMaxClicked()
+{
+    // 如果最大化，则变为正常，否则全部最大化
+    if (m_currentWidgetState == WidgetState::MaxState)
+    {
+        m_currentWidgetState = WidgetState::NomalState;
+        showNormal();
+    }
+    else
+    {
+        m_currentWidgetState = WidgetState::MaxState;
+        showMaximized();
+    }
+}
+
+void LWidget::onTitleBarDoubleClicked()
+{
+    // 如果最大化，则变为正常，否则全部最大化
+    onTitleBarBtnNomalOrMaxClicked();
 }
--- src/core/luicommon/lwidget.h
+++ src/core/luicommon/lwidget.h
@@ -2,7 +2,30 @@
 #define LWIDGET_H
 
 #include <QWidget>
+#include <QPointer>
 #include "luicommon_global.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// <summary>	窗口状态. </summary>
+///
+/// <remarks>	Liruilong, 2024/12/16. </remarks>
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+enum class WidgetState
+{
+    MinState = 0, // 最小化
+    MaxState,     // 最大化
+    NomalState,   // 正常大小
+    OtherState    // 其它大小
+};
+
+class LTitleBar;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// <summary>	实现一个自定义的QWidget. </summary>
+///
+/// <remarks>	Liruilong, 2024/12/16. </remarks>
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class LUICOMMON_EXPORT LWidget : public QWidget
 {
@@ -10,6 +33,33 @@ class LUICOMMON_EXPORT LWidget : public QWidget
 public:
     explicit LWidget(QWidget * parent = nullptr);
     virtual ~LWidget() override;
+
+    void initWidget();
+
+    void setWidgetState(WidgetState widgetState);
+    void connectInit();
+
+public slots:
+    void onTitleBarBtnCloseClicked();
+    void onTitleBarBtnMinClicked();
+    void onTitleBarBtnNomalOrMaxClicked();
+    void onTitleBarDoubleClicked();
+
+private:
+    WidgetState m_currentWidgetState{WidgetState::MaxState};
+
+    /*
+    widget结构
+     _____________________________
+    |                             |
+    |          标题栏             |
+    |          工具栏			  |
+    |          正文               |
+    |_____________________________|
+    */
+    LTitleBar *       m_pTitleBar{nullptr};     // 标题栏
+    QWidget *         m_pPalletWidget{nullptr}; // 工具栏
+    QPointer<QWidget> m_pCenterWidget{nullptr}; // 正文
 };
 
 #endif
